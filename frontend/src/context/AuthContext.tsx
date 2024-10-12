646f3ad02b89b34b87c2c85bc69f95198542358f
Ronald Dosunmu <ronaldosunmu@gmail.com>
2024-10-12 17:08:19 +0100

    implement auth context

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,60 @@
+import { jwtDecode } from "jwt-decode";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface AuthContextType {
+    token: string | null;
+    setToken: (token: string | null) => void;
+    logout: () => void;
+    login: (token: string) => void;
+    isAuthenticated?: boolean
+}
+
+interface User {
+    first_name: string;
+    last_name: string;
+    email: string;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
+    const [isAuthenticated, setIsAuthenticated] = useState(Boolean(localStorage.getItem('token')));
+    const [user, setUser] = useState<User>();
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        if (storedToken) {
+            setToken(storedToken);
+            setIsAuthenticated(true);
+            const decodedToken = jwtDecode(storedToken) as { user: User };
+            setUser(decodedToken.user);
+        }
+    }, []);
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setToken(null);
+        setIsAuthenticated(false);
+    };
+
+    const login = (newToken: string) => {
+        localStorage.setItem('token', newToken);
+        setToken(newToken);
+        setIsAuthenticated(true);
+    };
+
+    return (
+        <AuthContext.Provider value={{ token, setToken, logout, login, isAuthenticated, user }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+};
