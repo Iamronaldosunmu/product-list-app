b6d4fde8763e0c3f90532a91072834f24bea70cb
Ronald Dosunmu <ronaldosunmu@gmail.com>
2024-10-12 17:14:45 +0100

    implement api client

---
+++ frontend/src/api/apiClient.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+
+const BASE_URL = 'http://127.0.0.1:4000';
+
+console.log(BASE_URL);
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem('token');
+    if (token && config.headers) {
+      // Use direct assignment to set the Authorization header
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error('Unauthorized access, redirecting...');
+      localStorage.clear()
+      window.location.href = '/'
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { apiClient };
