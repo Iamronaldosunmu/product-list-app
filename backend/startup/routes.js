df2322a93df26472b5e1b4e3e6c79294ccc416d0
Ronald Dosunmu <ronaldosunmu@gmail.com>
2024-10-12 17:02:40 +0100

    add products router

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,8 +1,10 @@
 import authMiddleware from "../middleware/auth.js";
 import error from "../middleware/error.js";
+import productRouter from "../routes/product.js";
 import userRouter from "../routes/user.js";
 
 export default function (app) {
   app.use("/user", userRouter);
+  app.use("/products", productRouter);
   app.use(error);
 }
