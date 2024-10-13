b7deefd21385dc3830dd946668d4734f03dab98e
Ronald Dosunmu <ronaldosunmu@gmail.com>
2024-10-13 13:55:52 +0100

    change configuration for environment variable

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:4000';
+const BASE_URL = import.meta.env.VITE_BASE_URL ;
 
 console.log(BASE_URL);
 
